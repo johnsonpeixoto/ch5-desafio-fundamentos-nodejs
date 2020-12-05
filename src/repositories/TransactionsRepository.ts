c70eb38b5897165cecd483379a95c6d868ead616
johnsonpeixoto <johnson@fotonica.ifce.edu.br>
2020-12-05 02:39:20 -0300

    desafio feito e testado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const transactionsArray = this.transactions;
+    return transactionsArray;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const {income, outcome} = this.transactions.reduce((sum: Balance, record: CreateTransaction) => {
+      switch (record.type) {
+        case 'income':
+          sum.income += record.value
+          break;
+
+        case 'outcome':
+          sum.outcome += record.value
+          break;
+
+        default:
+          break;
+      }
+      return sum;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+
+    const total =  income - outcome;
+    return {income, outcome, total};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
